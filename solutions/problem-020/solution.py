5b7f67500b6646de7af1766cd1290b3088d475eb
André Vieira da Silva <sgavsnake@gmail.com>
2021-10-11 20:17:45 -0300

    added solution problem 007 in java

---
+++ solutions/problem-020/solution.py
@@ -0,0 +1,15 @@
+t=int(raw_input())
+i=0
+while(i<t):
+    n=int(raw_input())
+    j=1
+    k=1
+    s=0
+    while(j<=n):
+        k=k*j
+        j+=1
+    while(k):
+        s+=k%10
+        k=k/10
+    print(s)
+    i+=1
